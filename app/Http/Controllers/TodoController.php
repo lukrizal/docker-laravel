a6025d6dd72824c6da3ab45f4eaa888a7f971d32
Joseph Lacerna <joseph@lapapa88.com>
2017-04-05 12:02:56 +0800

    Setting the API

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        # Return all todos
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        # Validate request
+        # Insert todo item
+        # Return inserted item
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        # Validate item by locating it
+        # Return located item
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        # Validate item by locating it
+        # Validate request
+        # Update item
+        # Return updated item
+    }
+}
