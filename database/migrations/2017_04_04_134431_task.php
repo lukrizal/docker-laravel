d1a21fbb431cb4479aa7df800559e459f631c531
Joseph Lacerna <joseph@lapapa88.com>
2017-04-05 12:03:38 +0800

    Setting the Database

---
+++ database/migrations/2017_04_04_134431_task.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Task extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('task')->required()->unique();
+            $table->boolean('done')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropUnique('tasks_task_unique');
+        });
+        Schema::drop('tasks');
+    }
+}
