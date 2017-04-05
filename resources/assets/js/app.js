52a578b8c75e847b723afe42edea8e97c7a6fab9
Joseph Lacerna <joseph@lapapa88.com>
2017-04-05 12:07:01 +0800

    Connecting Front with API

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,86 @@
 window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 window.angular = require('angular');
+
+const app = angular.module('todoApp', []);
+
+app.config ['$interpolateProvider', ($interpolateProvider)=> {
+    $interpolateProvider.startSymbol('%%')
+    $interpolateProvider.endSymbol('%%')
+}]
+
+const todoCtrl = function($scope, api) {
+    $scope.list = [];
+    $scope.form = false;
+    $scope.data = { task: '', done: false, id: null };
+
+    api.get((res)=> {
+        $scope.list = [];
+        if (res.status === 200) {
+            $scope.list = res.data.collection;
+        }
+    }, (res)=> {
+        console.dir(res);
+    });
+
+    $scope.add = function addItem() {
+        api.add($scope.data, (res)=> {
+            if (res.status === 200) {
+                $scope.list.push(res.data.item);
+            }
+        }, (res)=> {
+            console.dir(res);
+        });
+    };
+
+    $scope.update = function updateItem(id, data) {
+        api.update(id, data, (res)=> {
+            if (res.status === 200) {
+                item = _.find($scope.list, { id })
+                angular.extend(item, data);
+            }
+        }, (res)=> {
+            console.dir(res);
+        });
+    };
+};
+
+const apiService = function($http) {
+    return {
+        url: '/api/todos',
+        request(opts, successCallback, errorCallback) {
+            return $http(opts).then((resp)=> {
+                successCallback(resp)
+            }, (resp)=> {
+                errorCallback(resp)
+            });
+        },
+        update(id, data, successCallback=_.noop, errorCallback=_.noop) {
+            const opts = {
+                url: `${this.url}/${id}`,
+                method: 'PUT',
+                data,
+            };
+            return this.request(opts, successCallback, errorCallback);
+        },
+        add(data, successCallback=_.noop, errorCallback=_.noop) {
+            const opts = {
+                url: this.url,
+                method: 'POST',
+                data,
+            };
+            return this.request(opts, successCallback, errorCallback);
+        },
+        get(successCallback=_.noop, errorCallback=_.noop) {
+            const opts = {
+                url: this.url,
+                method: 'GET',
+            };
+            return this.request(opts, successCallback, errorCallback);
+        },
+    };
+};
+
+app.controller('todoCtrl', ['$scope', 'api', todoCtrl])
+
+app.service('api', ['$http', apiService])
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,26 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="todoApp">
     <head>
         <meta charset="utf-8">
         <title>TODO App</title>
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <div>
+        <div ng-controller="todoCtrl">
             <h1 class="text-center">TO DO LIST</h1>
             <div class="new-wrapper">
-                <input type="text" value="New task">
-                <button class="btn btn-circle btn-info">+</button>
+                <input type="text" value="New task" ng-model="data.task">
+                <button class="btn btn-circle btn-info" ng-click="add()">+</button>
             </div>
 
             <ul class="list-group list-group-flush tasks">
-                <li class="list-group-item">
-                    <span>Not done task</span>
-                    <a href="#" class="btn btn-done glyphicon glyphicon-unchecked"></a>
-                </li>
-                <li class="list-group-item done">
-                    <span>Done task</span>
-                    <a href="#" class="btn btn-done glyphicon glyphicon-check"></a>
+                <li class="list-group-item" ng-class="{ done: item.done }" ng-repeat="item in list">
+                    <span ng-bind="item.task"></span>
+                    <a href="#" ng-click="update(item.id, { done: !item.done })" class="btn btn-done glyphicon" ng-class="{ 'glyphicon-check': item.done, 'glyphicon-unchecked': !item.done }"></a>
                 </li>
             </ul>
         </div>
